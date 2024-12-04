45315dc46dff24ef01eced8d96d3f8e0e953cf94
Jeremy Hertel <dev@jhert.com>
2024-12-03 22:52:44 -0600

    implement day 3 part 2

--- 2024/src/day-3.zig
+++ 2024/src/day-3.zig
@@ -1,13 +1,16 @@
 const std = @import("std");
+const ArrayListUnmanaged = std.ArrayListUnmanaged;
 
-pub fn main() !void {
+fn calculateTotal(part2: bool) !u32 {
+    var total: u32 = 0;
+
+    var run_mul = true;
+
     var file = try std.fs.cwd().openFile("inputs/day3.txt", .{});
     defer file.close();
 
     var buf_reader = std.io.bufferedReader(file.reader());
     var stream = buf_reader.reader();
-
-    var total: u32 = 0;
 
     const size = 4096;
     var buf: [size]u8 = [_]u8{0} ** size;
@@ -25,15 +28,34 @@ pub fn main() !void {
                 break;
             }
 
+            if (part2) {
+                if (!run_mul) {
+                    const do = std.mem.indexOfPos(u8, line, starting_index, "do()");
+                    if (do != null and do.? < mul_start.?) {
+                        run_mul = true;
+                    }
+                } else {
+                    const dont = std.mem.indexOfPos(u8, line, starting_index, "don't()");
+                    if (dont != null and dont.? < mul_start.?) {
+                        run_mul = false;
+                        starting_index = dont.? + 1;
+                        continue;
+                    }
+                }
+            }
+
             starting_index = mul_start.? + 1;
+
+            if (!run_mul) {
+                continue;
+            }
 
             const diff = mul_end.? - mul_start.?;
             if (diff > 11) {
                 continue;
             }
 
-            const slice = buf[mul_start.? + 4 .. mul_end.?];
-            std.debug.print("{s}\n", .{slice});
+            const slice = line[mul_start.? + 4 .. mul_end.?];
 
             var comma_found = false;
             for (slice) |c| {
@@ -57,5 +79,13 @@ pub fn main() !void {
         }
     }
 
-    std.debug.print("part 1: {any}\n", .{total});
+    return total;
+}
+
+pub fn main() !void {
+    const total = try calculateTotal(false);
+    const total2 = try calculateTotal(true);
+
+    std.debug.print("part 1: {d}\n", .{total});
+    std.debug.print("part 2: {d}\n", .{total2});
 }
